8a61fbddb57d2358fbf110064a71f70922f0ca59
RomdhoniZidane20 <73183067+RomdhoniZidane20@users.noreply.github.com>
2022-02-10 22:54:03 +0700

    Update Helloworld.cpp

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main(){
-  std::cout << "Hello World" << std::endl;
+  std::cout << "Hello GitHub" << std::endl;
+  std::cout << "Selamat Pagi" << std::endl;
   return 0;
 }
